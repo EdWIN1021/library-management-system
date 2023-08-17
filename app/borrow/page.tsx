74b45cce5173bda46c1de833929155f79c2c8b35
Edwin <>
2023-08-17 14:47:56 +0800

    test

--- app/borrow/page.tsx
+++ app/borrow/page.tsx
@@ -5,8 +5,10 @@ import dynamic from "next/dynamic";
 const BorrowList = dynamic(() => import("./BorrowList/BorrowList"));
 
 const BorrowPage = async () => {
-  const user = await getCurrentUser();
-  const borrowList = (await getBorrowList(user?.id)) || [];
+  // const user = await getCurrentUser();
+  // const borrowList = (await getBorrowList(user?.id)) || [];
+
+  const borrowList = (await getBorrowList("64d6d86bb4f00cb411cce575")) || [];
 
   return <BorrowList borrowList={borrowList} />;
 };
